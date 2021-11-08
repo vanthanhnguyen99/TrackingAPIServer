bfb5ac3b6f2616a28581a79ae05821c6d10571c6
vanthanhnguyen99 <vanthanhnguyen43@gmail.com>
2021-11-09 00:16:29 +0700

    update

--- src/main/java/com/vanth/controller/LoginController.java
+++ src/main/java/com/vanth/controller/LoginController.java
@@ -1,5 +1,6 @@
 package com.vanth.controller;
 
+import org.apache.catalina.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -8,13 +9,19 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.vanth.entity.Account;
+import com.vanth.entity.Users;
 import com.vanth.repository.AccountRepository;
+import com.vanth.repository.UserRepository;
+import com.vanth.request.RegistAccountRequest;
 
 @RestController
 public class LoginController 
 {
 	@Autowired
 	AccountRepository repo;
+	
+	@Autowired
+	UserRepository repoUsers;
 	
 	@PostMapping("/login")
 	public ResponseEntity<Object> login(@RequestBody Account account)
@@ -23,5 +30,41 @@ public class LoginController
 			return new ResponseEntity<Object>(false,HttpStatus.OK);
 				
 		return new ResponseEntity<Object>(true,HttpStatus.OK);
+	}
+	
+	@PostMapping("/regist")
+	public ResponseEntity<Object> registAccount(@RequestBody RegistAccountRequest registAccountRequest)
+	{
+		if (repo.existsById(registAccountRequest.getUsername()))
+		{
+			return new ResponseEntity<Object>("101", HttpStatus.BAD_REQUEST);
+		}
+			
+		
+		Users users = new Users();
+		Account account = new Account();
+		
+		account.setUsername(registAccountRequest.getUsername());
+		account.setPassword(registAccountRequest.getPassword());
+			
+		users.setName(registAccountRequest.getName());
+		users.setGender(registAccountRequest.isGender());
+		users.setPhone(registAccountRequest.getPhone());
+		users.setEmail(registAccountRequest.getEmail());
+		users.setAccount(account);
+		
+		// save data
+		try
+		{
+			repo.save(account);
+			repoUsers.save(users);
+		}
+		catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+			return new ResponseEntity<Object>("103",HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+		
+		return new ResponseEntity<Object>("200",HttpStatus.OK);
 	}
 }
